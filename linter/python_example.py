733b5666ee6bcb4b8a0404b5d6cd422eac6983e7
Maria Laranjeira <up202004453@up.pt>
2024-10-20 01:10:54 +0100

    Add python documentation

--- linter/python_example.py
+++ linter/python_example.py
@@ -1,6 +1,16 @@
+"""
+This module checks if all characters in a string are unique.
+"""
+
 def is_unique(
 			s
 			):
+	"""
+	Determines if a string has all unique characters.
+
+	:param s: The input string to check.
+	:return: 1 if all characters are unique, otherwise 0.
+	"""
 	s = list(s
 				)
 	s.sort()
@@ -9,8 +19,8 @@ def is_unique(
 	for i in range(len(s) - 1):
 		if s[i] == s[i + 1]:
 			return 0
-	else:
-		return 1
+
+	return 1
 
 
 if __name__ == "__main__":
