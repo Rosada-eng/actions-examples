3bc6ce36c23e2fa1858c21e92a9425261b1ab562
MariaLaranjeira <MariaLaranjeira@users.noreply.github.com>
2024-10-21 16:34:47 +0000

    chore: fix linting issues

--- linter/python_example.py
+++ linter/python_example.py
@@ -2,28 +2,23 @@
 This module checks if all characters in a string are unique.
 """
 
-def is_unique(
-			s
-			):
-	"""
-	Determines if a string has all unique characters.
 
-	:param s: The input string to check.
-	:return: 1 if all characters are unique, otherwise 0.
-	"""
-	s = list(s
-				)
-	s.sort()
+def is_unique(s):
+    """
+    Determines if a string has all unique characters.
 
+    :param s: The input string to check.
+    :return: 1 if all characters are unique, otherwise 0.
+    """
+    s = list(s)
+    s.sort()
 
-	for i in range(len(s) - 1):
-		if s[i] == s[i + 1]:
-			return 0
+    for i in range(len(s) - 1):
+        if s[i] == s[i + 1]:
+            return 0
 
-	return 1
+    return 1
 
 
 if __name__ == "__main__":
-	print(
-		is_unique(input())
-		)
+    print(is_unique(input()))
